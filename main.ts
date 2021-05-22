0ff37e79c97ce8aa6038fb865d18cc5d53906554
carla1364 <84620808+carla1364@users.noreply.github.com>
2021-05-22 10:36:45 +0100

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,26 @@
+// comentário
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . # . .
+        . # # # .
+        # . . . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . # . .
+        # . . . #
+        . # # # .
+        `)
+})
